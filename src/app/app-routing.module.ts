d1ccec8ba53de7b28a0de0a1126f88821f91bf6b
DarylMartinDipp <didieravangers@gmail.com>
2024-06-21 16:06:54 +0200

    Change the app-routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {EventListComponent} from "./components/event-list/event-list.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home', component: EventListComponent},
+  {path: '', redirectTo: '/home', pathMatch: 'full'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
