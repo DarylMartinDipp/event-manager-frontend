efa0f574eb49850393809d3cdbb4e2ff71a5e5b6
DarylMartinDipp <didieravangers@gmail.com>
2024-06-21 15:28:02 +0200

    Update the providers in app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {TopBarComponent} from "./components/top-bar/top-bar.component";
+import {HttpClientModule} from "@angular/common/http";
+import {CategoryService} from "./services/category.service";
+import {UserService} from "./services/user.service";
+import {EventService} from "./services/event.service";
 
 @NgModule({
   declarations: [
@@ -12,9 +16,14 @@ import {TopBarComponent} from "./components/top-bar/top-bar.component";
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
   ],
-  providers: [],
+  providers: [
+    CategoryService,
+    UserService,
+    EventService,
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
