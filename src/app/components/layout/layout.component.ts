3d18e522e967474cec08a520cb95311a891e8ccc
pneveu <pneveu937@gmail.com>
2024-06-29 21:15:42 +0200

    top bar and layout ts update

--- src/app/components/layout/layout.component.ts
+++ src/app/components/layout/layout.component.ts
@@ -1,1 +1,8 @@
-// layout TS
+import {Component} from "@angular/core";
+
+@Component({
+  selector: 'app-layout',
+  templateUrl: './layout.component.html',
+  styleUrls: ['./layout.component.css']
+})
+export class LayoutComponent {}
--- src/app/components/top-bar/top-bar.component.ts
+++ src/app/components/top-bar/top-bar.component.ts
@@ -1,8 +1,23 @@
 import { Component } from "@angular/core";
+import {Router} from "@angular/router";
 
 @Component({
   selector: "app-top-bar",
   templateUrl: "./top-bar.component.html",
   styleUrls: ["./top-bar.component.scss"],
 })
-export class TopBarComponent {}
+export class TopBarComponent {
+  loggedUser: any;
+
+  constructor(private router: Router) {
+    const localUser = localStorage.getItem('loggedUser');
+    if (localUser != null) {
+      this.loggedUser = JSON.parse(localUser)
+    }
+  }
+
+  onLogOut() {
+    localStorage.removeItem('loggedUser');
+    this.router.navigate(['/signIn']);
+  }
+}
