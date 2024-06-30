93977f950ec833f7a9d3ff5a0d15a94a75667d85
DarylMartinDipp <didieravangers@gmail.com>
2024-06-30 22:07:50 +0200

    Add registration data model

--- src/app/data/registration.ts
+++ src/app/data/registration.ts
@@ -1,1 +1,14 @@
-// Registration data
+import {User} from "./user";
+import {Event} from "./event";
+
+export interface Registration {
+  id: string;
+  user_id: User;
+  event_id: Event;
+  registered_at: Date;
+}
+
+export type RegistrationCreateInput = Omit<Registration, "id" | "user_id" | "event_id" | "registered_at"> & {
+  userId: String;
+  eventId: String;
+};
